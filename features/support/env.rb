b40d3c2a3c59a144d49b8accc54b0c8ca8baf386
Rafael Pereira De Godoi Romero <rpromero@stefanini.com>
2018-05-07 13:40:55 -0300

    Commit: Updating the "Capybara.default_max_wait_time = 2" to "= 10". To guarantee if the internet conection is low.

--- features/support/env.rb
+++ features/support/env.rb
@@ -5,5 +5,5 @@ Capybara.register_driver :selenium do |app|
 Capybara::Selenium::Driver.new(app, :browser => :chrome) 
 end 
 Capybara.default_driver = :selenium 
-Capybara.default_max_wait_time = 2
+Capybara.default_max_wait_time = 10
 
