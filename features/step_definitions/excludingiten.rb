09432ede96c0bf9038e5dd40e9a41e17f6c5d122
Rafael Pereira De Godoi Romero <rpromero@stefanini.com>
2018-05-07 13:12:23 -0300

    Commit: Deleting a commentary line.

--- features/step_definitions/excludingiten.rb
+++ features/step_definitions/excludingiten.rb
@@ -1,7 +1,4 @@
-  
   Then("i exclude one iten from the cart") do
    find(".basket-productRemoveAct").click
    find(".basket-empty > h2", text: "sua cesta está vazia")
   end
-  
-  #app > section > section > div.basket-productsAndFreight__wrapper > section > h2
